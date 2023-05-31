f88afe89234f67521bf10ed89ab1b1ab7a92db25
Drayko <draykom@gmail.com>
2023-05-31 17:05:28 +1000

    Moved static functions from CLI to TaskFunctions

--- CAB301_Assignment_3/CLI.cs
+++ CAB301_Assignment_3/CLI.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Globalization;
 using System.Runtime.CompilerServices;
 using System.Threading.Tasks;
 
@@ -6,8 +7,6 @@ namespace ProjectManagementSystem
 {
     internal class CLI
     {
-        private static List<Task> _tasks = new List<Task>();
-        private static Dictionary<string, Task> _taskDict = new Dictionary<string, Task>();
         private enum Choice
         {
             EXIT,
@@ -20,145 +19,26 @@ namespace ProjectManagementSystem
         };
         private const string TITLE = "================ Project Management System ================";
         private const string PAD = "> ";
-        private static string? fileName = "";
-        private static bool isChanged = false;
+        private static bool s_isChanged = false;
         public static void IncorrectInput()
         {
             Console.WriteLine(" ~ The provided input was of the incorrect type ~ \n ~  Press any key to return to the home screen  ~ \n...");
             Console.ReadKey();
             Console.Clear();
         }
-        public static void LoadTasks(string[] tasksText)
-        {
-            string[][] taskItemsArray = (from taskString in tasksText select taskString.Split(new[] { ',' }, 3)).ToArray();
-
-            // First loop - init all tasks
-            foreach (string[] taskItems in taskItemsArray)
-            {
-                Task task = new Task(taskItems[0]);
-                _taskDict.Add(taskItems[0], task);
-                _tasks.Add(task);
-            }
-
-            // Second loop: updates details for all tasks
-            for (int i = 0; i < tasksText.Length; i++)
-            {
-                Task task = _tasks[i];
-
-                uint timeToCompletion = uint.Parse(taskItemsArray[i][1]); // Add try/catch {|!|}
-
-                // Getting dependencies from text to obj
-                HashSet<Task> dependencies = new HashSet<Task>();
-                if (taskItemsArray[i].Length == 3) // If the task has dependencies
-                {
-                    string[] dependenciesString = taskItemsArray[i][2].Split(",");
-
-                    foreach (string dependencyID in dependenciesString)
-                    {
-                        _taskDict.TryGetValue(dependencyID.Trim(), out Task dependency);
-                        dependencies.Add(dependency);
-                    }
-                }
-                task.Update(timeToCompletion, dependencies);
-            }
-        }
         public static void ListTasks()
         {
             Console.Clear();
             Console.WriteLine(TITLE);
-            Console.WriteLine($"== File: {fileName}{(isChanged? "*" : "")}\n");
+            Console.WriteLine($"== File: {TaskFunctions.s_fileName}{(s_isChanged ? "*" : "")}\n");
             Console.WriteLine("------------ Tasks ------------>");
-            _tasks.ForEach(Console.WriteLine);
+            TaskFunctions.Tasks.ForEach(Console.WriteLine);
             Console.WriteLine("------------------------------->");
             Console.WriteLine();
-        }
-        public static void AddTask()
-        {
-            Console.WriteLine(PAD + "What is the ID of the task you want to add?");
-            Console.Write("ID:");
-            string? id = Console.ReadLine();
-
-            Console.WriteLine(PAD + "How long does it take to complete this task?");
-            Console.Write("Time:");
-            uint timeToCompletion = uint.Parse(Console.ReadLine());
-
-            Console.WriteLine(PAD + "List the tasks that would need to be completed before you can start this task.\nEnter each task ID seperated by a comma (',') ");
-            Console.Write("Tasks:");
-            string[] dependenciesString = Console.ReadLine().Split(',');
-
-            HashSet<Task> dependencies = new HashSet<Task>();
-
-            foreach (string dependencyID in dependenciesString)
-            {
-                _taskDict.TryGetValue(dependencyID.Trim(), out Task dependency);
-                dependencies.Add(dependency);
-            }
-
-            Task task = new Task(id, timeToCompletion, dependencies);
-            _tasks.Add(task);
-            _taskDict.Add(id, task);
-        }
-        public static void DeleteTask()
-        {
-            Console.WriteLine(PAD + "What is the ID of the task you want to remove?");
-            Console.Write("ID:");
-            string? idToDelete = Console.ReadLine();
-
-            _taskDict.TryGetValue(idToDelete.Trim(), out Task taskToDelete);
-
-            foreach (Task task in _tasks)
-            {
-                task.DeleteDependency(taskToDelete);
-            }
-            _tasks.Remove(taskToDelete);
-            _taskDict.Remove(idToDelete);
-        }
-        public static void DeleteTask(List<Task> tasks, Task taskToDelete)
-        {
-            foreach (Task task in tasks)
-            {
-                task.DeleteDependency(taskToDelete);
-            }
-            tasks.Remove(taskToDelete);
-        }
-        public static void ChangeTime()
-        {
-            Console.WriteLine(PAD + "What is the ID of the task you want to update?");
-            Console.Write("ID:");
-            string? id = Console.ReadLine();
-            Console.WriteLine(PAD + "What is the new time to complete this task?");
-            Console.Write("Time:");
-            uint newTime = uint.Parse(Console.ReadLine());
-
-            _taskDict.TryGetValue(id.Trim(), out Task task);
-            task.ChangeTime(newTime);
-        }
-        public static void Save()
-        {
-            string[] taskStrings = (from task in _tasks select task.FormatFileString()).ToArray();
-            File.WriteAllLines(fileName, taskStrings);
-        }
-        public static List<Task> Sequence()
-        {
-            List<Task> topOrdering = new List<Task>();
-
-            List<Task> sCopyTasks = new List<Task>(_tasks);
-            while (sCopyTasks.Count != 0)
-            {
-                Task v = (from freeTask in sCopyTasks where !freeTask.HasDependencies select freeTask).First();
-                topOrdering.Add(v);
-                DeleteTask(sCopyTasks, v);
-            }
-            return topOrdering;
         }
 
         public static void Main()
         {
-            /*
-            Ask the user to enter the name of a text file in which the information about the tasks in
-            a project and the dependencies among the tasks are stored ...
-            */
-
             string[]? tasksText = null;
 
             while (tasksText is null)
@@ -167,11 +47,11 @@ namespace ProjectManagementSystem
                 Console.WriteLine(TITLE + "\n");
                 Console.WriteLine(PAD + "Enter the name of the file that contains your tasks:");
                 Console.Write("Name:");
-                fileName = Console.ReadLine();
+                TaskFunctions.s_fileName = Console.ReadLine();
 
                 try
                 {
-                    tasksText = File.ReadAllLines(fileName);
+                    tasksText = File.ReadAllLines(TaskFunctions.s_fileName);
                 }
                 catch (FileNotFoundException)
                 {
@@ -184,7 +64,7 @@ namespace ProjectManagementSystem
             and read the information
             from the text file into the system.
             */
-            LoadTasks(tasksText);
+            TaskFunctions.LoadTasks(tasksText);
 
             while (true)
             {
@@ -210,7 +90,7 @@ namespace ProjectManagementSystem
                 switch (choice)
                 {
                     case (int)Choice.EXIT:
-                        if (isChanged)
+                        if (s_isChanged)
                         {
                             ListTasks();
                             Console.WriteLine(PAD + "You have unsaved changes. Do you want save them before exiting?");
@@ -235,31 +115,54 @@ namespace ProjectManagementSystem
                         break;
 
                     case (int)Choice.ADD:
-                        AddTask();
-                        isChanged = true;
+                        Console.WriteLine(PAD + "What is the ID of the task you want to add?");
+                        Console.Write("ID:");
+                        string? idToAdd = Console.ReadLine();
+
+                        Console.WriteLine(PAD + "How long does it take to complete this task?");
+                        Console.Write("Time:");
+                        uint timeToCompletion = uint.Parse(Console.ReadLine());
+
+                        Console.WriteLine(PAD + "List the tasks that would need to be completed before you can start this task.\nEnter each task ID seperated by a comma (',') ");
+                        Console.Write("Tasks:");
+                        string[] dependenciesIDs = Console.ReadLine().Split(',');
+
+                        TaskFunctions.AddTask(idToAdd, timeToCompletion, dependenciesIDs);
+                        s_isChanged = true;
                         break;
 
                     case (int)Choice.DELETE:
-                        DeleteTask();
-                        isChanged = true;
+                        Console.WriteLine(PAD + "What is the ID of the task you want to remove?");
+                        Console.Write("ID:");
+                        string? idToDelete = Console.ReadLine();
+
+                        TaskFunctions.DeleteTask(idToDelete);
+                        s_isChanged = true;
                         break;
 
                     case (int)Choice.CHANGETIME:
-                        ChangeTime();
-                        isChanged = true;
+                        Console.WriteLine(PAD + "What is the ID of the task you want to update?");
+                        Console.Write("ID:");
+                        string? idToChange = Console.ReadLine();
+                        Console.WriteLine(PAD + "What is the new time to complete this task?");
+                        Console.Write("Time:");
+                        uint newTime = uint.Parse(Console.ReadLine());
+
+                        TaskFunctions.ChangeTime(idToChange, newTime);
+                        s_isChanged = true;
                         break;
 
                     case (int)Choice.SAVE:
-                        Save();
-                        isChanged = false;
+                        TaskFunctions.Save();
+                        s_isChanged = false;
                         Console.WriteLine("Task saved successfully\nPress any key to continue\n...");
                         Console.ReadKey();
                         break;
 
                     case (int)Choice.SEQUENCE:
-                        List<Task> topOrdering = Sequence();
-                        Console.WriteLine($"Sequence: [ { String.Join(", ", topOrdering)} ]\n ... has been saved to Sequence.txt");
-                        
+                        List<Task> topOrdering = TaskFunctions.Sequence();
+                        Console.WriteLine($"Sequence: [ { String.Join(", ", topOrdering)} ]\n ... has been saved to Sequence.txt\nPress any key to continue\n...");
+                        Console.ReadKey();
                         break;
 
                     case (int)Choice.EARLIESTTIMES:
---
+++ CAB301_Assignment_3/TaskFunctions.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectManagementSystem
+{
+    internal class TaskFunctions
+    {
+        public static List<Task> Tasks = new List<Task>();
+        internal static Dictionary<string, Task> s_taskDict = new Dictionary<string, Task>();
+        internal static string? s_fileName = "";
+
+        public static void LoadTasks(string[] tasksText)
+        {
+            string[][] taskItemsArray = (from taskString in tasksText select taskString.Split(new[] { ',' }, 3)).ToArray();
+
+            // First loop - init all tasks
+            foreach (string[] taskItems in taskItemsArray)
+            {
+                Task task = new Task(taskItems[0]);
+                s_taskDict.Add(taskItems[0], task);
+                Tasks.Add(task);
+            }
+
+            // Second loop: updates details for all tasks
+            for (int i = 0; i < tasksText.Length; i++)
+            {
+                Task task = Tasks[i];
+
+                uint timeToCompletion = uint.Parse(taskItemsArray[i][1]); // Add try/catch {|!|}
+
+                // Getting dependencies from text to obj
+                HashSet<Task> dependencies = new HashSet<Task>();
+                if (taskItemsArray[i].Length == 3) // If the task has dependencies
+                {
+                    string[] dependenciesString = taskItemsArray[i][2].Split(",");
+
+                    foreach (string dependencyID in dependenciesString)
+                    {
+                        s_taskDict.TryGetValue(dependencyID.Trim(), out Task dependency);
+                        dependencies.Add(dependency);
+                    }
+                }
+                task.Update(timeToCompletion, dependencies);
+            }
+        }
+        public static void AddTask(string id, uint timeToCompletion, string[] dependenciesIDs)
+        {
+            HashSet<Task> dependencies = new HashSet<Task>();
+
+            foreach (string dependencyID in dependenciesIDs)
+            {
+                s_taskDict.TryGetValue(dependencyID.Trim(), out Task dependency);
+                dependencies.Add(dependency);
+            }
+
+            Task task = new Task(id, timeToCompletion, dependencies);
+            Tasks.Add(task);
+            s_taskDict.Add(id, task);
+        }
+        public static void DeleteTask(string idToDelete)
+        {
+            s_taskDict.TryGetValue(idToDelete.Trim(), out Task taskToDelete);
+
+            foreach (Task task in Tasks)
+            {
+                task.DeleteDependency(taskToDelete);
+            }
+            Tasks.Remove(taskToDelete);
+            s_taskDict.Remove(idToDelete);
+        }
+        public static void DeleteTask(List<Task> tasks, Task taskToDelete)
+        {
+            foreach (Task task in tasks)
+            {
+                task.DeleteDependency(taskToDelete);
+            }
+            tasks.Remove(taskToDelete);
+        }
+        public static void ChangeTime(string id, uint newTime)
+        {
+            s_taskDict.TryGetValue(id.Trim(), out Task task);
+            task.ChangeTime(newTime);
+        }
+        public static void Save()
+        {
+            string[] taskStrings = (from task in Tasks select task.FormatFileString()).ToArray();
+            File.WriteAllLines(s_fileName, taskStrings);
+        }
+        public static List<Task> Sequence()
+        {
+            List<Task> topOrdering = new List<Task>();
+
+            List<Task> sCopyTasks = new List<Task>(Tasks);
+            while (sCopyTasks.Count != 0)
+            {
+                Task v = (from freeTask in sCopyTasks where !freeTask.HasDependencies select freeTask).First();
+                topOrdering.Add(v);
+                DeleteTask(sCopyTasks, v);
+            }
+            return topOrdering;
+        }
+    }
+}
